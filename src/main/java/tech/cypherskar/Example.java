f27e3afc54547659b522e2ba4971367b5d73f6f2
cypherskar <islamnasri21@gmail.com>
2021-05-09 04:41:38 +0300

    fixed some trailing whitespaces

--- src/main/java/tech/cypherskar/Example.java
+++ src/main/java/tech/cypherskar/Example.java
@@ -38,19 +38,17 @@ public class Example
     {
         // [1]: Create and Configure Datacenters and Hosts with -> Datacenters.
         Datacenters.List().Name("Server").Architecture("x86", "Linux", "Xen").
-        //TimeZone(10.0, 15).SecCost(3.0, 10).MemCost(0.05).StorageCost(0.001).BwCost(0.0).Create(10);
         TimeZone(10.0).SecCost(3.0).MemCost(0.05).StorageCost(0.001).BwCost(0.0).Create(1);
         
         // Hosts.
         Datacenters.Hosts().Mips(177730).Pes(6).Ram(16000).Bw(15000).Storage(4000000).Create(2, "Server");
-        //Datacenters.Hosts().Mips(220000).Pes(1).Ram(16000).Bw(10000).Storage(4000000).Create(2, "Server");
         
         // [2]: Create and Configure Brokers, Virtual Machines and Cloudlets (Tasks) with -> Brokers.
         Brokers.List().Name("Cypher").Create();
         
         // Virtual Machines.
         Brokers.Vms().Mips(9726).Pes(1).Ram2(9).Bw(1000).Image(10000).Vmm("Xen").Create(5);
-        
+
         // Cloudlets (Tasks).
         Brokers.SetTasksSplitMode(TasksSplit.Even);
         Brokers.Tasks().Length(10000, 20000).Pes(1).FileSize(1).OutpuSize(1).Create(RandomStyle.Fixed_Pace);
@@ -67,25 +65,26 @@ public class Example
         // Set Workload Folder To Use External Jobs Data if any (i.e. CEA, Planetlab...etc)
         Simulations.WorkloadFolder(System.getProperty("user.dir"));
 
-		// [5]: Setup Results Preferances (i.e. font size, background color...etc) -> Window.
-		// Setup View Monitor/Screen
+        // [5]: Setup Results Preferances (i.e. font size, background color...etc) -> Window.
+        // Setup View Monitor/Screen
         Window.ShowOnScreen(1);
-		// Setup Tables
+        
+        // Setup Tables
         Window.SetTableHintEnds('(', ')');
         Window.SetTableCellSeparator('|');
         Window.SetTableMode(TableMode.Compact_With_TasksSplit);
         Window.SetTableBackground(Color.BLACK);
         Window.SetTableForeground(Color.WHITE);
-        
-		// Setup Charts
-		Window.SetChartTheme(ChartTheme.XChart);
-		Window.SetChartSize(0,0); // Automatic resizing if (0,0), also affects export image size
-		Window.SetChartRenderStyle(CategorySeriesRenderStyle.Bar);
-		Window.SetChartLegend(LegendPosition.OutsideS, LegendLayout.Horizontal);
+
+        // Setup Charts
+        Window.SetChartTheme(ChartTheme.XChart);
+        Window.SetChartSize(0,0); // Automatic resizing if (0,0), also affects export image size
+        Window.SetChartRenderStyle(CategorySeriesRenderStyle.Bar);
+        Window.SetChartLegend(LegendPosition.OutsideS, LegendLayout.Horizontal);
         Window.SetChartAnnotation(true, 90);
         Window.SetChartYAxisTickDivider(6);
         Window.SetChartGridLinesVisible(true, false);
-		Window.SetChartImageFormat(BitmapFormat.PNG);
+        Window.SetChartImageFormat(BitmapFormat.PNG);
 
         // [6]: Set CloudSim Settings And Sign Your Experiment -> Experiment.
         Experiment.CloudsimLogState(false);
